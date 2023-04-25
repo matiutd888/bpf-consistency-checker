4e01007fee0849212adf1de3a02ddfc4d2444412
Mateusz <matiutd888@gmail.com>
2023-04-25 02:09:08 +0200

    change to rsync

--- send-local-changes.sh
+++ send-local-changes.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Change to the local repository directory
-cd ~/studia-current/zso/zso-2
-
-# Get the timestamp of the last commit
-last_commit_time=$(git log -1 --format=%ct)
-
-# Find all files changed since the last commit
-changed_files=$(git diff --name-only HEAD)
-echo $changed_files
-
-for f in $changed_files
-do
-    echo $f
-    scp $f zso-root:zso-2/$f
-done
+# # Change to the local repository directory
+# cd ~/studia-current/zso/zso-2
+# 
+# # Get the timestamp of the last commit
+# last_commit_time=$(git log -1 --format=%ct)
+# 
+# # Find all files changed since the last commit
+# changed_files=$(git diff --name-only HEAD)
+# echo $changed_files
+# 
+# for f in $changed_files
+# do
+#     echo $f
+#     rsync -ru $f zso-root:zso-2/$f
+# done
 
 # Send the changed files to the remote server using scp
 # scp $changed_files zso-root:zso-2/$changed_files
-
+rsync -iruvhP z2-tst-public zso-root:zso-2/
+echo "test synced"
+rsync -iruvhP linux-6.2.1 zso-root:zso-2/
